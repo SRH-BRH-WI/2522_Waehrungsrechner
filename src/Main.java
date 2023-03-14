dadd246bd667ae7c40bc13ce509f18322b31a3f5
Dr. Dillinger <peter.dillinger@srh.de>
2023-03-14 13:50:27 +0100

    Name der Währung als Attribut der Klasse ergänzt.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        Währungsrechner dollar = new Währungsrechner(1.06);
 
-        System.out.println("100 Euro entsprechen " + dollar.inFremd(100) + " Dollar");
-        System.out.println("100 Dollar entsprechen " + dollar.inEuro(100) + " Euro");
+    public static void ausgabe(Währungsrechner w, int betrag) {
+        System.out.println(betrag + " Euro entsprechen " + w.inFremd(betrag) + " " + w.getName());
+        System.out.println(betrag + " " + w.getName() + " entsprechen " + w.inEuro(betrag) + " Euro");
+    }
+
+
+    public static void main(String[] args) {
+        Währungsrechner dollar = new Währungsrechner("Dollar", 1.06);
+        Währungsrechner yen = new Währungsrechner("Yen", 142.88);
+
+        ausgabe(dollar, 100);
+        ausgabe(yen, 100);
     }
 }
--- src/Währungsrechner.java
+++ src/Währungsrechner.java
@@ -1,8 +1,14 @@
 public class Währungsrechner {
+    private String name;
     private double kurs;
 
-    public Währungsrechner(double kurs) {
+    public Währungsrechner(String name, double kurs) {
+        this.name = name;
         this.kurs = kurs;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public double inFremd(double betrag) {
